79290c7de142fa2273612f08b550e8d854209323
Teemo Tebest <teemo.tebest@gmail.com>
2022-07-26 09:55:30 +0200

    Updated scripts to build and to sync

--- templates/react-webpack/webpack.common.js
+++ templates/react-webpack/webpack.common.js
@@ -72,10 +72,14 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'assets/img/', to: '../public/assets/img/', noErrorOnMissing: true},
-        { from: 'assets/data/data.json', to: '../public/assets/data/data.json', noErrorOnMissing: true},
-        { from: 'src/font/', to: '../public/font/', noErrorOnMissing: true},
-        { from: './favicon.png', to: '../public', noErrorOnMissing: true}
+        { from: 'assets/img/', to: '../public/assets/img/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
+          }},
+        { from: 'assets/data/data.json', to: '../public/assets/data/data.json', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
+          }},
+        { from: 'src/font/', to: '../public/font/', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
+          }},
+        { from: './favicon.png', to: '../public', noErrorOnMissing: true, globOptions: { dot: true, gitignore: true, ignore: ['**/.DS_Store'],
+          }}
       ]
     })
   ]
