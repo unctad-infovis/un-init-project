3fe77486b07a9663bd04cd37b849f1f4d1bf7caf
Teemo Tebest <teemo.tebest@gmail.com>
2022-09-16 15:38:04 +0200

    Fixed template

--- templates/react-webpack/src/index.js
+++ templates/react-webpack/src/index.js
@@ -4,6 +4,6 @@ import { createRoot } from 'react-dom/client';
 
 import App from './jsx/App.jsx';
 
-const container = document.getElementById('__PROJECT_NAME__');
+const container = document.getElementById('app-root-__PROJECT_NAME__');
 const root = createRoot(container);
 root.render(<App />);
