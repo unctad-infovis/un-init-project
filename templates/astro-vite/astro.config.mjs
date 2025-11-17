bf1cdd4db7ebfd6559e36f6fb9088996feb833d4
Teemo Tebest <teemo.tebest@gmail.com>
2025-11-17 10:59:03 +0100

    Updated

--- templates/astro-vite/astro.config.mjs
+++ templates/astro-vite/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import less from '@astrojs/renderer-less';
 
 export default defineConfig({
+  renderers: [less()],
   vite: {
     logLevel: 'error', // hides warnings
   },
