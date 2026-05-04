c100c8d004b0ec9a5e6ac5278fd7daf9bc4b7dcb
Teemo Tebest <teemo.tebest@gmail.com>
2026-05-04 18:56:45 +0200

    Fixed

--- templates/react-vite/vite.config.js
+++ templates/react-vite/vite.config.js
@@ -1,36 +1,37 @@
-import { createRequire } from "node:module";
-import { defineConfig } from "vite";
+import { createRequire } from 'node:module';
+import { defineConfig } from 'vite';
 
 const require = createRequire(import.meta.url);
-const { name } = require("./package.json");
+const { name } = require('./package.json');
 
 export default defineConfig(({ command }) => ({
-	server: {
-		hot: true,
-		open: true,
-	},
-	build: {
-		emptyOutDir: true,
-		outDir: "dist",
-		minify: "terser",
-		rollupOptions: {
-			input: {
-				index: "./index.html",
-			},
-			output: {
-				entryFileNames: `js/${name}.min.js`,
-				chunkFileNames: `js/${name}.[name].js`,
-				assetFileNames: (assetInfo) => {
-					if (assetInfo.name?.endsWith(".css")) return `css/${name}.min.css`;
-					return `assets/[name][extname]`;
-				},
-			},
-		},
-		sourcemap: true,
-		terserOptions: {
-			compress: {
-				drop_console: command === "build",
-			},
-		},
-	},
+  base: `/${name}/`,
+  server: {
+    hot: true,
+    open: true
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist',
+    minify: 'terser',
+    rollupOptions: {
+      input: {
+        index: './index.html'
+      },
+      output: {
+        entryFileNames: `js/${name}.min.js`,
+        chunkFileNames: `js/${name}.[name].js`,
+        assetFileNames: assetInfo => {
+          if (assetInfo.name?.endsWith('.css')) return `css/${name}.min.css`;
+          return `assets/[name][extname]`;
+        }
+      }
+    },
+    sourcemap: true,
+    terserOptions: {
+      compress: {
+        drop_console: command === 'build'
+      }
+    }
+  }
 }));
